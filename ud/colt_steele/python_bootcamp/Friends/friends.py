d23c5328797d91e2afaa1cdcefad536dad63313e
RickyMcMaster <ricky.mcmaster@gmail.com>
2021-07-08 15:47:16 +0200

    up to end of databases

--- ud/colt_steele/python_bootcamp/Friends/friends.py
+++ ud/colt_steele/python_bootcamp/Friends/friends.py
@@ -1,7 +1,14 @@
 import sqlite3
 
+# Create connection
 conn = sqlite3.connect("my_friends.db")
 c = conn.cursor()
-c.execute(
-    "CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+# c.execute(
+#     "CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+insert_query = '''
+INSERT INTO friends VALUES ('jack', 'sprat', 7);
+'''
+c.execute(insert_query)
+# Commit
+conn.commit()
 conn.close()
